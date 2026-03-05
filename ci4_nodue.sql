29fa3478379bd07cb4499141370211fe7f51cdab
abiramiv14 <146544816+abiramiv14@users.noreply.github.com>
2026-03-05 18:36:07 +0530

    Add files via upload

---
+++ ci4_nodue.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 05, 2026 at 02:05 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4_nodue`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+-- Error reading structure for table ci4_nodue.departments: #1932 - Table &#039;ci4_nodue.departments&#039; doesn&#039;t exist in engine
+-- Error reading data for table ci4_nodue.departments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ci4_nodue`.`departments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department_clearances`
+--
+-- Error reading structure for table ci4_nodue.department_clearances: #1932 - Table &#039;ci4_nodue.department_clearances&#039; doesn&#039;t exist in engine
+-- Error reading data for table ci4_nodue.department_clearances: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ci4_nodue`.`department_clearances`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table ci4_nodue.migrations: #1932 - Table &#039;ci4_nodue.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table ci4_nodue.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ci4_nodue`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_requests`
+--
+-- Error reading structure for table ci4_nodue.service_requests: #1932 - Table &#039;ci4_nodue.service_requests&#039; doesn&#039;t exist in engine
+-- Error reading data for table ci4_nodue.service_requests: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ci4_nodue`.`service_requests`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+-- Error reading structure for table ci4_nodue.students: #1932 - Table &#039;ci4_nodue.students&#039; doesn&#039;t exist in engine
+-- Error reading data for table ci4_nodue.students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ci4_nodue`.`students`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table ci4_nodue.users: #1932 - Table &#039;ci4_nodue.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table ci4_nodue.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ci4_nodue`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
